459c60580c00020aa77bcbabf362b3dbfd1e0037
Aklilegetachew <aklilegetachew@gmail.com>
2024-11-08 18:11:34 +0300

    making changes to video and ico

--- src/app/Component/HeroPage.tsx
+++ src/app/Component/HeroPage.tsx
@@ -12,7 +12,7 @@ export default function Component() {
           playsInline
           className="h-full w-full object-cover border-2"
         >
-          <source src="/tomocaFarmHero.mp4" type="video/mp4" />
+          <source src="/Tomocafarmhero.mp4" type="video/mp4" />
         </video>
         <div className="absolute inset-0 bg-black/60" />
       </div>
